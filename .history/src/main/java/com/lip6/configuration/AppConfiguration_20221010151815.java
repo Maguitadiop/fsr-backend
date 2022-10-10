67547bd02b4862eecdab21145e18826f972f677e
hajarsalhi <shajar32@gmail.com>
2022-10-10 20:23:12 +0200

    debug cors policy

--- .history/src/main/java/com/lip6/configuration/AppConfiguration_20221010151815.java
+++ .history/src/main/java/com/lip6/configuration/AppConfiguration_20221010151815.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-//si je veux intï¿½grer une config XML venant de mon legacy, j'utilise l'@ImportResource 
+//si je veux intégrer une config XML venant de mon legacy, j'utilise l'@ImportResource 
 //@ImportResource(locations = {"classpath:spring/app-context-xml.xml"})
 
 @ImportResource(locations = {"classpath:applicationContext.xml"})
@@ -13,7 +13,4 @@ import org.springframework.context.annotation.ImportResource;
 public class AppConfiguration {
 
 	//plus rien dans la classe Java, juste une indication ou scanner les beans
-
-	
-
 }
